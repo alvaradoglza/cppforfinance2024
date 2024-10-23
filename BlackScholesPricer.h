03f21bf096ade74ebbc9591f57cb7c47238dcf40
Einarpth <einarp.th@hotmail.com>
2024-10-23 15:50:36 +0000

    declaring black and scholes pricer

---
+++ BlackScholesPricer.h
@@ -0,0 +1,26 @@
+// =========================================================
+// File: BlackScholesPricer.h
+// Author: 
+// Date: [23:10.2025]
+// Description: Header file for BlackScholesPricer class
+// =========================================================
+
+
+class BlackScholesPricer (EuropeanVanillaOption* option, double asset_price,
+ double interest_rate, double volatility)
+{
+private:
+    /* data */
+public:
+    BlackScholesPricer(/* args */);
+    ~BlackScholesPricer();
+};
+
+BlackScholesPricer::BlackScholesPricer(/* args */)
+{
+}
+
+BlackScholesPricer::~BlackScholesPricer()
+{
+}
+
