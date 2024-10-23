5af0800f137dae11ef8532b5b0795317cf1e2fd7
Emilio Alvarado González Aragón <65573248+alvaradoglza@users.noreply.github.com>
2024-10-23 15:50:12 +0000

    call option created

--- CallOption.h
+++ CallOption.h
@@ -1,1 +1,29 @@
-// test
+// =========================================================
+// File: CallOption.h
+// Author: 
+// Date: 23/10/2024
+// Description: Header file for CallOption class
+// =========================================================
+#ifndef CALLOPTION_H
+#define CALLOPTION_H
+
+#include "europeanVanillaOption.h" // Include the base class
+
+class CallOption : public EuropeanVanillaOption {
+public:
+
+    // Constructor
+    CallOption(double expiry, double strike) : EuropeanVanillaOption(expiry, strike) {}
+
+    // Implement the payoff function for Put Option
+    double payoff(double z) const override {
+        return (z >= getStrike()) ? (z - getStrike()) : 0;
+    }
+
+    // Return the option type (Call)
+    optionType GetOptionType() const override {
+        return optionType::call;
+    }
+};
+
+#endif
