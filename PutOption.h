be2e6ebc2072f34579b1cbeb4bb57162aee4cda8
Emilio Alvarado González Aragón <65573248+alvaradoglza@users.noreply.github.com>
2024-10-23 15:14:23 +0000

    class for PutOption creation

---
+++ PutOption.h
@@ -0,0 +1,28 @@
+// =========================================================
+// File: putOption.h
+// Author: 
+// Date: 23/10/2024
+// Description: Header file for PutOption class
+// =========================================================
+#ifndef PUTOPTION_H
+#define PUTOPTION_H
+
+#include "europeanVanillaOption.h" // Include the base class
+
+class PutOption : public EuropeanVanillaOption {
+public:
+    // Constructor
+    PutOption(double expiry, double strike) : EuropeanVanillaOption(expiry, strike) {}
+
+    // Implement the payoff function for Put Option
+    double payoff(double z) const override {
+        return (z <= getStrike()) ? (getStrike() - z) : 0;
+    }
+
+    // Return the option type (Put)
+    optionType GetOptionType() const override {
+        return optionType::put;
+    }
+};
+
+#endif
