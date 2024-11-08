ebce238818638d5878f5eb155ddddc80a56cc395
Ghali Benboubker <125286409+GhaliBenboubker@users.noreply.github.com>
2024-11-08 13:26:03 +0000

    a

---
+++ BinaryTree.h
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+
+template <typename T>
+class BinaryTree {
+private:
+    int _depth;  
+    std::vector<std::vector<T>> _tree;  
+
+public:
+    
+    void setDepth(int depth) {
+        _depth = depth;
+        _tree.resize(_depth + 1);
+        for (int i = 0; i <= _depth; ++i) {
+            _tree[i].resize(i + 1);
+        }
+    }
+
+    
+    void setNode(int n, int i, T value) {
+        _tree[n][i] = value;
+    }
+
+    
+    T getNode(int n, int i) const {
+        return _tree[n][i];
+    }
+
+    
+    void display() const {
+        for (int n = 0; n <= _depth; ++n) {
+            for (int i = 0; i <= n; ++i) {
+                std::cout << "Node(" << n << ", " << i << "): " << _tree[n][i] << std::endl;
+            }
+        }
+    }
+};
