03976db26298487ce91ca29dead269d864379646
Emilio Alvarado González Aragón <65573248+alvaradoglza@users.noreply.github.com>
2024-11-12 10:43:06 +0000

    changes we finished of part 2 o 3 during class but i forgot to push

--- BinaryTree.h
+++ BinaryTree.h
@@ -31,9 +31,50 @@ public:
 
     
     void display() const {
+        int max_width = _depth + 1;  // Maximum width of the tree
+        int spacing = 4;            // Spacing between levels for formatting
+    
         for (int n = 0; n <= _depth; ++n) {
+            // Print leading spaces for alignment
+            int offset = (max_width - n) * spacing / 2;
+            for (int i = 0; i < offset; ++i) {
+                std::cout << " ";
+            }
+    
+            // Print values at level `n`
             for (int i = 0; i <= n; ++i) {
-                std::cout << "Node(" << n << ", " << i << "): " << _tree[n][i] << std::endl;
+                std::cout << _tree[n][i];
+                // Add spaces between nodes at the same level
+                if (i < n) {
+                    for (int j = 0; j < spacing; ++j) {
+                        std::cout << " ";
+                    }
+                }
+            }
+            std::cout << std::endl;
+    
+            // Print slashes (`/` and `\`) to connect levels
+            if (n < _depth) {
+                // Print leading spaces for the slashes
+                int slash_offset = (max_width - n - 1) * spacing / 2;
+                for (int i = 0; i < slash_offset; ++i) {
+                    std::cout << " ";
+                }
+    
+                // Print slashes between nodes
+                for (int i = 0; i <= n; ++i) {
+                    std::cout << "/";
+                    for (int j = 0; j < spacing - 1; ++j) {
+                        std::cout << " ";
+                    }
+                    std::cout << "\\";
+                    if (i < n) {
+                        for (int j = 0; j < spacing - 1; ++j) {
+                            std::cout << " ";
+                        }
+                    }
+                }
+                std::cout << std::endl;
             }
         }
     }
